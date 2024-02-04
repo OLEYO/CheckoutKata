a474096f2de14dac874069e8c6cfa4b23c1721d4
Krzysztof Skolas <thecomputerguy.44@gmail.com>
2024-02-04 17:06:07 +0000

    begun work. scoping out necessary classes and planning methods out.

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -8,7 +8,38 @@ namespace CheckoutKata
 {
     public class Checkout : ICheckout
     {
+        //  store the pricing rules and scanned items in a list
+        private readonly List<PricingRule> pricingRules;
+        private readonly List<ScannedItem> scannedItems;
 
+        // constructor to recieve pricingRules and initialize a new list for scanned items
+        public Checkout(List<PricingRule> pricingRules)
+        {
+            this.pricingRules = pricingRules;
+            this.scannedItems = new List<ScannedItem>();
+        }
+
+        public void Scan(string item)
+        {
+        }
     }
 
+    public class PricingRule
+    {
+        public string Item { get; }
+        public int UnitPrice { get; }
+        public (int Quantity, int SpecialPrice) SpecialOffer { get; }
+    }
+    public class ScannedItem
+    {
+        public string Item { get; }
+        public int Quantity { get; set; }
+
+
+        public ScannedItem(string item)
+        {
+            Item = item;
+            Quantity = 1;
+        }
+    }
 }
