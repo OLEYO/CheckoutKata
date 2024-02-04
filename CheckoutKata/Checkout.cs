84aea1ba7ad0f0255ab9b205ffaadf0de0e9c7a8
Krzysztof Skolas <thecomputerguy.44@gmail.com>
2024-02-04 17:28:20 +0000

    finished gettotalprice method and price rule calculating. introduced comments to explain workings

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CheckoutKata
 {
@@ -28,36 +26,95 @@ namespace CheckoutKata
             {
                 var scannedItem = GetScannedItem(item);
 
+                //check if the item already exists and add quantity if it does
                 if (scannedItem != null) 
                     scannedItem.Quantity++;
                 
+                //if the item doesnt exist then add to scannedItems
                 else 
                     scannedItems.Add(new ScannedItem(item));
 
             }
-            else Console.WriteLine($"Error: Item '{item}' not found in pricing rules.");
+            //error if the item doesnt exist in pricing rules 
+            else 
+                Console.WriteLine($"Error: Item '{item}' not found in pricing rules.");
             
         }
+
         public int GetTotalPrice()
         {
+            //explicitly set totalPrice to 0 
             int totalPrice = 0;
-        }
+
+            //foreach scanned item add to the total price, while checking if the items qualify for special price.
+            foreach (var scannedItem in scannedItems)
+            {
+                var pricingRule = GetPricingRule(scannedItem.Item);
+
+                if (pricingRule != null)
+                    totalPrice += pricingRule.CalculateTotalPrice(scannedItem.Quantity);
+
+                //error when pricing rules cant be found for a scanned item
+                else
+                    Console.WriteLine($"Error: Pricing rules for item '{scannedItem.Item}' not found.");
+                
+            }
+
             return totalPrice;
-    }
+        }
+
+        private PricingRule GetPricingRule(string item)
+        {
+            return pricingRules.FirstOrDefault(rule => rule.Item == item);
+        }
 
         private ScannedItem GetScannedItem(string item)
         {
             return scannedItems.FirstOrDefault(scannedItem => scannedItem.Item == item);
         }
+    }
+
     public class PricingRule
     {
-        public string Item { get; }
-        public int UnitPrice { get; }
-        public (int Quantity, int SpecialPrice) SpecialOffer { get; }
+        public string Item { get; } //name of the item that the rule applies to
+        public int UnitPrice { get; } //normal price of item
+        public (int Quantity, int SpecialPrice) SpecialOffer { get; } //quantity required to get special price, and said special price
+
+        public PricingRule(string item, int unitPrice, (int Quantity, int SpecialPrice) specialOffer)
+        {
+            Item = item; 
+            UnitPrice = unitPrice; 
+            SpecialOffer = specialOffer; 
+        }
+
+        public int CalculateTotalPrice(int quantity)
+        {
+            int regularPrice = quantity * UnitPrice;
+
+            //check if the pricing rule has a special offer 
+            if (SpecialOffer.Quantity > 0)
+            {
+
+                //how many items under special offer price 
+                int specialPriceSets = quantity / SpecialOffer.Quantity;
+
+                //how many items remain to be under normal price
+                int remainderQuantity = quantity % SpecialOffer.Quantity;
+
+                //calculating price total price based off items that qualified for special offer and ones that did not.
+                int specialPriceTotal = (specialPriceSets * SpecialOffer.SpecialPrice) + (remainderQuantity * UnitPrice);
+
+                return specialPriceTotal;
+            }
+
+            //return regular price if theres no special offer
+            return regularPrice;
+        }
     }
+
     public class ScannedItem
     {
-        public string Item { get; }
+        public string Item { get; } // item name
         public int Quantity { get; set; }
 
 
